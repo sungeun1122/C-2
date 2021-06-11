ae2a253dbb8f1cd5eefc1c2b70600f0e0360bf8d
admin <admin@DESKTOP-R2NKGM0>
2021-06-11 20:00:17 +0900

    초기 커밋.

---
+++ 01_Intro/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h> // malloc, rand, atoi .. 동적메모리, 난수, 문자열 변환
+#include <string.h> // strcpy .. 문자열 함수.  memcpy.. 메모리블럭 함수
+#include <conio.h>  // _getch, putch .. MS 기반 C비표준 함수
+#include <time.h>   // clock .. 시간 날짜 함수
+#pragma warning(disable:4996)  // scanf() 등 전통 C함수중 버퍼문제.
+#pragma warning(disable:4477)  // unsigned <--> signed 관련 warning
+
+/*
+*
+*
+*/
+
+int main()
+{
+	// TODO : 코드를 작성하세요
+
+	printf("\n아무키나 입력하면 프로그램 종료됩니다\n");
+	_getch();
+	return 0;
+}
